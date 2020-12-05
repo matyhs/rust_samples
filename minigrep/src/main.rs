42171318c1fcfb1899891d6c49b4c4e3d31ea058
Matthew Que <matthew.neale.que@gmail.com>
2020-12-05 16:08:21 +0800

    minigrep sample exercise

---
+++ minigrep/src/main.rs
@@ -0,0 +1,17 @@
+use std::env;
+use std::process;
+use minigrep::Config;
+
+fn main() {
+    let args : Vec<String> = env::args().collect();
+
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+
+    if let Err(error) = minigrep::run(config) {
+        eprintln!("Application error: {}", error);
+        process::exit(1);
+    }
+}
